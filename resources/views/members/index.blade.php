98e25f7c47270b900873a22a725174894cfd25df
infoharry99 <infoharry99@gmail.com>
2026-04-21 17:43:04 +0530

    Sd

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -328,7 +328,7 @@
                 </div>
 
                 <!-- Submit -->
-                <div class="col-md-6 mb-3 d-flex align-items-end">
+                <div class="col-md-6  d-flex align-items-end">
                     <button type="submit" class="gradient-btn" id="submitBtn">
                         Add Member
                     </button>
